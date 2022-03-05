167dd7278865524ae49672f5c8c1236088a0b00b
Mitch Earnshaw <mitchell.earnshaw@service.nsw.gov.au>
2022-03-05 12:49:46 +1100

    Setup

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import type {Node} from 'react';
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const App: () => Node = () => {};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
